e0644eb477bc7c6701a66baf830b1d93512f0c42
Jose Chaparro <josechaparro@Joses-MacBook-Pro.local>
2021-07-31 14:08:30 -0700

    red border and error message are removed once fields are full and submit button is clicked

--- dist/js/app.js
+++ dist/js/app.js
@@ -12,49 +12,33 @@ function showError(input, message) {
   small.innerText = message;
 }
 
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-input success';
+}
+
 // Check if email is valid
 function checkEmail(input){
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if(re.test(input.value.trim())){
 
-    // showSuccess(input);
+    showSuccess(input);
   } else {
     showError(input, 'Email is not valid')
   }
 }
-
-// Check password match
-// function checkPasswordsMatch(input1, input2) {
-//   if(input1.value !== input2.value){
-//     showError(input2, 'Passwords do not match');
-//   }
-// }
-
-// Show success outline
-// function showSuccess(input) {
-//   const formControl = input.parentElement;
-//   formControl.className = 'form-control success';
-// }
 
 // Check Required Fields
 function checkRequired(inputArr){
   inputArr.forEach(function(input){
     if(input.value.trim() === ''){
       showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
     }
   });
 }
-
-// Check input lengths
-// function checkLength(input, min, max) {
-//   if(input.value.length < min ) {
-//     showError(input, `${getFieldName(input)} must be at least ${min} characters`);
-//   } else if (input.value.length > max){
-//     showError(input, `${getFieldName(input)} must be less than ${max} characters`);
-//   } else {
-//     showSuccess(input);
-//   }
-// }
 
 // Get Field Name
 function getFieldName(input) {
